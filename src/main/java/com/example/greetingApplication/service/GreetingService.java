00395c8b608fc5510784aed49d50c1d60b25f46c
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-22 21:48:37 +0530

    [Amber] Add. Modified GreetingService to generate personalized messages

--- src/main/java/com/example/greetingApplication/service/GreetingService.java
+++ src/main/java/com/example/greetingApplication/service/GreetingService.java
@@ -7,7 +7,15 @@ public class GreetingService {
 
     private String greetingMessage = "Hello World";
 
-    public String getGreeting() {
+    // Method to get a personalized greeting
+    public String getGreeting(String firstName, String lastName) {
+        if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()) {
+            return "Hello, " + firstName + " " + lastName + "!";
+        } else if (firstName != null && !firstName.isEmpty()) {
+            return "Hello, " + firstName + "!";
+        } else if (lastName != null && !lastName.isEmpty()) {
+            return "Hello, Mr./Ms. " + lastName + "!";
+        }
         return greetingMessage;
     }
 
