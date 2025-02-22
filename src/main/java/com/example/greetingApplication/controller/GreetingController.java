ce210bb9f6a9e162ee74f5f10bf1f0954cafed3d
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-22 21:44:04 +0530

    Merged uc2 into main

--- src/main/java/com/example/greetingApplication/controller/GreetingController.java
+++ src/main/java/com/example/greetingApplication/controller/GreetingController.java
@@ -1,36 +1,39 @@
 package com.example.greetingApplication.controller;
 
+import com.example.greetingApplication.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    private String greetingMessage = "Hello, Welcome to Greeting App!";
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     // GET request - Returns a Simple greeting message
     @GetMapping
     public String getGreeting() {
-        return greetingMessage;
+        return greetingService.getGreeting();
     }
 
     // POST request - Accepts a user-provided message and returns it
     @PostMapping
     public String createGreeting(@RequestBody String message) {
-        return "Greeting received: " + message;
+        return greetingService.createGreeting(message);
     }
 
     // PUT request - Updates the greeting message
     @PutMapping
     public String updateGreeting(@RequestBody String message) {
-        this.greetingMessage = message;
-        return "Greeting updated successfully!";
+        return greetingService.updateGreeting(message);
     }
 
     // DELETE request - Deletes the greeting message
     @DeleteMapping
     public String deleteGreeting() {
-        this.greetingMessage = "No greeting available.";
-        return "Greeting deleted successfully!";
+        return greetingService.deleteGreeting();
     }
 }
